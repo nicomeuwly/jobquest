818813a0687b6613571dbe078d1d261a237ea3a5
Nicolas Meuwly <nicomeuwly@gmail.com>
2025-02-05 11:02:13 +0100

    change list element style and add titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center m-8">
       <h1 className="text-2xl font-bold mb-8">JobQuest 🔍</h1>
+      <div className="w-full px-6 py-4 rounded-lg flex items-center flex-row">
+        <h2 className="basis-1/2 text-gray-400">Descriptif du poste</h2>
+      </div>
       <ListItem />
     </div>
   );
