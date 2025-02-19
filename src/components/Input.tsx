f8c94d006b273d2125bc316e18d03be5c62db6c5
Nicolas Meuwly <nicomeuwly@gmail.com>
2025-02-19 20:21:57 +0100

    fix input placeholder background when focus

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -25,7 +25,7 @@ export default function InputElement({ placeholder, type, required, colSpan, val
 
     return (
         <div className={"relative" + (colSpan ? ` col-span-${colSpan}` : "")}>
-            <input type={type} id="floating_outlined" className={"block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-lg border-1 border-gray-500 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer hover:bg-gray-50 " + (inputValue ? "text-black" : "text-gray-500")} placeholder=" " name={name} required={required} defaultValue={value} />
+            <input type={type} id="floating_outlined" className={"block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-lg border-1 border-gray-500 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer hover:bg-gray-50 " + (inputValue ? "text-black" : "text-gray-500")} placeholder=" " name={name} required={required} value={inputValue} onChange={handleChange}/>
             <label htmlFor="floating_outlined" className={"absolute text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:bg-white peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1 " + (inputValue ? "bg-white" : "")}>{placeholder}</label>
         </div>
     );
